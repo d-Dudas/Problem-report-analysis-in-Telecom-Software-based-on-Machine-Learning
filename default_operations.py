d67ebc098baa80d0640f73dc65adc5eb26efdf37
DumeaGabriel <gabriel.dumea02@e-uvt.ro>
2023-03-22 20:21:49 +0200

    Added most_faulty_prontos and modified for CNN

--- default_operations.py
+++ default_operations.py
@@ -71,7 +71,35 @@ def list_tested_features():
 
     print(tested_features)
 
-# TO DO : software release with most faulty prontos? - most_faulty_release.py
+
+# This function shows the software with the most faulty prontos
+def most_faulty_prontos():
+    key = "release"
+    #we use a dictionary to remember the number of all faulty prontos for each software
+    releases={}
+    maxvalue = 0
+
+    for pronto in prontos_collection.find():
+
+        #We check for all faulty prontos
+        if pronto["state"]=="Correction Not Needed":
+        # pronto[key] is a list: ['SBTS00']
+        # isn't clear if this list could have multiple values
+        # so we assume that it could have, for worst case scenario 
+            for release in pronto[key]:
+                if release in releases.keys():
+                    releases[release] += 1
+                    if(releases[release] > maxvalue): 
+                        maxvalue = releases[release]
+                else:
+                    releases[release] = 1
+
+    # It is possible for two different releases
+    # to have the same amount of faulty prontos
+    print("Release(s) with most faulty prontos:",end=' ')
+    for i in releases.keys():
+        if(releases[i] == maxvalue):
+            print(i)
 
 # This function calculates the occurence percentages
 # for a specified (key) pronto state
--- most_faulty_realease.py
+++ most_faulty_realease.py
@@ -2,26 +2,32 @@ from pymongo import MongoClient
 
 #connect to db
 prontos_collection = MongoClient().prontosdb.prontos_collection
-releases = {}
-key = "release"
-maxvalue = 0
 
-for pronto in prontos_collection.find():
-    # pronto[key] is a list: ['SBTS00']
-    # isn't clear if this list could have multiple values
-    # so we assume that it could have, for worst case scenario 
-    # + if state : CNN
-    for release in pronto[key]:
-        if release in releases.keys():
-            releases[release] += 1
-            if(releases[release] > maxvalue): 
-                maxvalue = releases[release]
-        else:
-            releases[release] = 1
 
-# It is possible for two different releases
-# to have the same amount of faulty prontos
-print("Release(s) with most faulty prontos:")
-for i in releases.keys():
-    if(releases[i] == maxvalue):
-        print(i)
+def most_faulty_prontos():
+    key = "release"
+    releases={}
+    maxvalue = 0
+
+    for pronto in prontos_collection.find():
+        # pronto[key] is a list: ['SBTS00']
+        # isn't clear if this list could have multiple values
+        # so we assume that it could have, for worst case scenario 
+        # + if state : CNN
+        if pronto["state"]=="Correction Not Needed":
+            for release in pronto[key]:
+                if release in releases.keys():
+                    releases[release] += 1
+                    if(releases[release] > maxvalue): 
+                        maxvalue = releases[release]
+                else:
+                    releases[release] = 1
+
+    # It is possible for two different releases
+    # to have the same amount of faulty prontos
+    print("Release(s) with most faulty prontos:",end=' ')
+    for i in releases.keys():
+        if(releases[i] == maxvalue):
+            print(i)
+
+most_faulty_prontos()
