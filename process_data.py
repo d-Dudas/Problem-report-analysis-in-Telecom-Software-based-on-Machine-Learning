ce46b28c46b11da3cd35c4d0313cc0ddef0960f4
d-Dudas <david.dudas03@e-uvt.ro>
2023-05-17 20:06:44 +0300

    Conditioned retraining model. Conditioned saving retrained model

--- process_data.py
+++ process_data.py
@@ -1,4 +1,3 @@
-import time
 import nltk
 import scipy.sparse as sp
 from threading import Thread
@@ -11,30 +10,26 @@ from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction import DictVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
 
-last_ml_model_update = time.time()
-re_train_ml_model_thread = None
+retrain_ml_model_thread = None
+recently_added_prontos = sld.deserialize_object("recently_uploaded_prontos.pickle")
+# When 5 or more prontos where uploaded, then retrain the model
+retrain_model_limit = 5
 
 #Filter and keep only the words that are in the english dictionary
 def filter_tokens(tokenized_descriptions):
     english_words = set(nltk.corpus.words.words())
     valid_tokens_list = []
     for tokens in tokenized_descriptions:
-        # Create a new list to store the valid English words for this description
         valid_tokens = []
-        # Loop through each token in the list
         for token in tokens:
-            # Check if the token is a valid English word
             if token.lower() in english_words:
-                # If it is, add it to the valid_tokens list
                 valid_tokens.append(token)
-        # Add the list of valid tokens for this description to the valid_tokens_list
         valid_tokens_list.append(valid_tokens)
 
     return valid_tokens_list
 
 # Stemm each token from the tokenized descriptions list
 def stemm_tokens(tokenized_descriptions, stemmer = nltk.PorterStemmer()):
-    # The stemmed tokens will be stored in the following array
     stemmed_descriptions = []
     stemmer = SnowballStemmer("english")
     for iter in tokenized_descriptions:
@@ -44,7 +39,6 @@ def stemm_tokens(tokenized_descriptions, stemmer = nltk.PorterStemmer()):
 
 # Rejoin stemmed tokens
 def rejoin_processed_tokens(tokens_list):
-    # The rejoined text is stored in the following array
     stemmed_text = []
     for tokens in tokens_list:
         stemmed_text.append(" ".join(tokens))
@@ -53,14 +47,9 @@ def rejoin_processed_tokens(tokens_list):
 
 # stemmed_text - result of rejoin_processed_tokens()
 def get_tfidfVector(stemmed_text):
-    # Vectorize the stemmed text
     vectorizer = TfidfVectorizer(stop_words='english')
-    return vectorizer.fit_transform(stemmed_text), vectorizer
 
-def fget_tfidfVector(stemmed_text):
-    # Vectorize the stemmed text
-    f = TfidfVectorizer(stop_words='english')
-    return f, f.fit(stemmed_text)
+    return vectorizer.fit_transform(stemmed_text), vectorizer
 
 # Returned the processed pronto title and descriptions
 # stored in form of a tfidf vector
@@ -75,7 +64,7 @@ def get_title_desc_vector():
             )
         )
 
-# Thats for new prontos
+# For new prontos
 def prepare_tdv(aux):
     return rejoin_processed_tokens(
             stemm_tokens(
@@ -89,11 +78,13 @@ def prepare_tdv(aux):
 # use with useful features list
 def get_dict_vector(data_list):
     dict_vectorizer = DictVectorizer(sparse=True)
+
     return dict_vectorizer.fit_transform(data_list), dict_vectorizer
 
 # Returns encoded data
 def encode_data(data):
     label_encoder = preprocessing.LabelEncoder()
+
     return label_encoder.fit_transform(data), label_encoder
 
 def get_fast_prediction(pronto):
@@ -121,27 +112,14 @@ def get_fast_prediction(pronto):
     return prediction[0], accuracy
 
 def try_retrain_ml_model():
-    current_time = time.time()
-    global last_ml_model_update
-    global re_train_ml_model_thread
-    if((current_time - last_ml_model_update) / 3600 >= 72 and (re_train_ml_model_thread is None or not re_train_ml_model_thread.is_alive())):
-        last_ml_model_update = current_time
-        re_train_ml_model_thread = Thread(target=re_train_ml_model, args=())
-        re_train_ml_model_thread.start()
-    else:
-        print("The ML model was updated in the last 72 hours, or the update is still running")
-
-def try_retrain_ml_model_now():
-    global last_ml_model_update
-    global re_train_ml_model_thread
-    if re_train_ml_model_thread is None or not re_train_ml_model_thread.is_alive():
-        last_ml_model_update = time.time()
-        re_train_ml_model_thread = Thread(target=re_train_ml_model, args=())
-        re_train_ml_model_thread.start()
+    global retrain_ml_model_thread
+    if retrain_ml_model_thread is None or not retrain_ml_model_thread.is_alive():
+        retrain_ml_model_thread = Thread(target=retrain_ml_model, args=())
+        retrain_ml_model_thread.start()
     else:
         print("The ML model is already updating...")
 
-def re_train_ml_model():
+def retrain_ml_model():
     # Read the data from database and save the new processed data
     print("Retraining ML model")
     title_and_desc_vec, vectorizer = get_title_desc_vector()
@@ -160,19 +138,28 @@ def re_train_ml_model():
     model_input = sp.hstack([title_and_desc_vec, useful_feature_list])
 
     # Split the data into test and train sets
-    x_train, x_test, y_train, y_test = train_test_split(model_input.toarray(), encoded_states, test_size = 0.7, random_state=50)
+    x_train, x_test, y_train, y_test = train_test_split(model_input.toarray(), encoded_states, test_size = 0.8, random_state=50)
 
     # Prepare the ml model and save the trained model and its accuracy
     ml_model = GaussianNB()
     ml_model.fit(x_train, y_train)
     accuracy = ml_model.score(x_test, y_test)
-    sld.serialize_object("MLmodel.pickle", ml_model)
-    sld.serialize_object("accuracy.pickle", accuracy)
+    prev_accuracy = sld.deserialize_object("accuracy.pickle")
+    if accuracy > prev_accuracy:
+        print("Better model trained.")
+        sld.serialize_object("MLmodel.pickle", ml_model)
+        sld.serialize_object("accuracy.pickle", accuracy)
+    else :
+        print("Retrained model not accurate enough.")
 
     print("ML model retrained.")
+    print("Accuracy: {}".format(accuracy))
 
 def upload_pronto(pronto):
     if sld.upload_pronto(pronto):
-        try_retrain_ml_model_now()
+        recently_added_prontos += 1
+        sld.serialize_object("recently_uploaded_prontos.pickle", recently_added_prontos)
+        if recently_added_prontos >= retrain_model_limit:
+            try_retrain_ml_model()
 
-try_retrain_ml_model_now()
+try_retrain_ml_model()
--- save_and_load_data.py
+++ save_and_load_data.py
@@ -3,36 +3,35 @@ from scipy.sparse import save_npz, load_npz
 from pymongo import MongoClient
 import pymongo
 
-
-
 def save_sparse_matrix(filename, matrix):
     filename = "./data/" + filename
     save_npz(filename, matrix)
 
 def load_sparse_matrix(filename):
     filename = "./data/" + filename
+
     return load_npz(filename)
 
-def serialize_object(filename, enc_data):
+def serialize_object(filename, data):
     filename = "./data/" + filename
     with open(filename, 'wb') as f:
-        pickle.dump(enc_data, f)
+        pickle.dump(data, f)
 
 def deserialize_object(filename):
     filename = "./data/" + filename
     with open(filename, 'rb') as f:
         object = pickle.load(f)
+
     return object
 
 def upload_pronto(pronto):
     prontos_collection = MongoClient().prontosdb.prontos_collection
-    uploaded = None
+    uploaded = False
     try:
         rez = prontos_collection.insert_one(pronto)
         print("Inserted pronto: {}".format(rez.inserted_id))
         uploaded = True
     except pymongo.errors.DuplicateKeyError:
         print("Duplicated key error for problemReportId = {}".format(pronto["problemReportId"]))
-        uploaded = False
 
     return uploaded
